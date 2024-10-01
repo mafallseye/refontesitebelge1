c3e5e488d6cd02127f79d7c28e694e89145e0b77
mafallseye <maguettefseye@gmail.com>
2024-10-01 19:16:38 +0200

    first commit

--- src/components/Projetbody.vue
+++ src/components/Projetbody.vue
@@ -89,9 +89,9 @@
 
                 <ul class="flex flex-col justify-between p-4 leading-normal">
                     <li class="a">amendement ineral basique :
-                        <ul class="flex flex-col justify-between p-4 leading-normal">
+                        <ul class="flex flex-col justify-between p-2 leading-normal">
                             <li class="b">correction de l'acidité (↑ pH):
-                                <ul class="flex flex-col justify-between p-4 leading-normal">
+                                <ul class="flex flex-col justify-between p-2 leading-normal">
                                     <li class="c"> les éléments nutritifs principaux (N-P-K-Ca-Mg-S) sont mieux assimilables
                                     </li>
                                     <li class="c"> le pH correspond aux besoins des plantes</li>
