4d7b04e05c33d17992f86ace0c80f6a9d2cf22c0
mafallseye <maguettefseye@gmail.com>
2024-10-01 17:44:02 +0200

    first commit

--- src/components/Projetbody.vue
+++ src/components/Projetbody.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="w-full p-4 text-center bg-blue-900 border border-gray-200   sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+    <div class="w-full p-4 text-center bg-green-600 border border-gray-200   sm:p-8 dark:bg-gray-800 dark:border-gray-700">
         <h5 class="mb-5 text-3xl font-bold text-white dark:text-white uppercase border-b">Projet</h5>
         <p class="mb-5 text-base text-white sm:text-lg dark:text-white">Le segment agricole subsaharien fait face à des
             défis majeurs suite aux :</p>
         <div class="items-center justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4 rtl:space-x-reverse">
             <a href="#"
-                class="w-full no-underline sm:w-auto bg-blue-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
+                class="w-full no-underline sm:w-auto bg-green-400 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
                 <!-- <svg class="me-3 w-7 h-7" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="apple" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"></path></svg> -->
                 <div class="text-left rtl:text-right">
                     <!-- <div class="mb-1 text-xs">Download on the</div> -->
@@ -13,7 +13,7 @@
                 </div>
             </a>
             <a href="#"
-                class="w-full sm:w-auto no-underline bg-blue-700 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
+                class="w-full sm:w-auto no-underline bg-green-400 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
                 <!-- <svg class="me-3 w-7 h-7" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google-play" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M325.3 234.3L104.6 13l280.8 161.2-60.1 60.1zM47 0C34 6.8 25.3 19.2 25.3 35.3v441.3c0 16.1 8.7 28.5 21.7 35.3l256.6-256L47 0zm425.2 225.6l-58.9-34.1-65.7 64.5 65.7 64.5 60.1-34.1c18-14.3 18-46.5-1.2-60.8zM104.6 499l280.8-161.2-60.1-60.1L104.6 499z"></path></svg> -->
                 <div class="text-left rtl:text-right">
                     <!-- <div class="mb-1 text-xs">Get in on</div> -->
@@ -22,7 +22,7 @@
                 </div>
             </a>
             <a href="#"
-                class="w-full sm:w-auto no-underline bg-blue-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
+                class="w-full sm:w-auto no-underline bg-green-400 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
                 <!-- <svg class="me-3 w-7 h-7" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google-play" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M325.3 234.3L104.6 13l280.8 161.2-60.1 60.1zM47 0C34 6.8 25.3 19.2 25.3 35.3v441.3c0 16.1 8.7 28.5 21.7 35.3l256.6-256L47 0zm425.2 225.6l-58.9-34.1-65.7 64.5 65.7 64.5 60.1-34.1c18-14.3 18-46.5-1.2-60.8zM104.6 499l280.8-161.2-60.1-60.1L104.6 499z"></path></svg> -->
                 <div class="text-left rtl:text-right">
                     <!-- <div class="mb-1 text-xs">Get in on</div> -->
@@ -35,7 +35,7 @@
 
     <!-- ====================image============================== -->
 
-    <section class="bg-white dark:bg-gray-900  no-border">
+    <section class="bg-white dark:bg-gray-900  no-border italic ">
         <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
             <div class="mr-auto place-self-center lg:col-span-7">
                 <h1 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">Le groupe Carmeuse (Terracalco),
@@ -62,13 +62,13 @@
             <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
 
 
-                <div class=" bg-blue-600">
-                    <a href="https ://www.carmeuse.com/eu-en/products/agriculture-feed-and-food/terracalco">
+                <div class=" bg-white">
+                    <a href="https://www.carmeuse.com/eu-en/products/agriculture-feed-and-food/terracalco">
                         <img class="" src="../imgtanzania/ph.png" alt="" />
                     </a>
                     <div class="">
-                        <a href="https ://www.carmeuse.com/eu-en/products/agriculture-feed-and-food/terracalco">
-                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-white dark:text-white">Terracalco </h5>
+                        <a href="https://www.carmeuse.com/eu-en/products/agriculture-feed-and-food/terracalco"  class="no-underline">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-green-600 dark:text-white">Terracalco </h5>
                         </a>
                         <!-- <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p> -->
                         <!-- <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
@@ -82,30 +82,30 @@
 
 
             </div>
-            <div class="mr-auto place-self-center lg:col-span-7 ">
+            <div class="mr-auto place-self-center lg:col-span-7 italic  ">
                 <h1 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">Le calcaire, la dolomie et la chaux ont un
                     impact majeur bien connu sur cette fertilité car ils opèrent en tant que :</h1>
                 <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
 
                 <ul>
-                    <li>amendement ineral basique :
+                    <li class="a">amendement ineral basique :
                         <ul>
-                            <li>correction de l’acidité (↑ pH):
+                            <li class="b">correction de l’acidité (↑ pH):
                                 <ul>
-                                    <li> les éléments nutritifs principaux (N-P-K-Ca-Mg-S) sont mieux assimilables
+                                    <li class="c"> les éléments nutritifs principaux (N-P-K-Ca-Mg-S) sont mieux assimilables
                                     </li>
-                                    <li> le pH correspond aux besoins des plantes</li>
-                                    <li>l’activité biologique dans le sol est plus intense</li>
+                                    <li class="c"> le pH correspond aux besoins des plantes</li>
+                                    <li class="c">l’activité biologique dans le sol est plus intense</li>
                                 </ul>
                             </li>
-                            <li> amélioration de la structure du sol</li>
+                            <li class="b"> amélioration de la structure du sol</li>
                         </ul>
                     </li>
-                    <li>engrais : apport en éléments nutritifs (calcium and magnésium) indispensables aux plantes</li>
+                    <li class="a">engrais : apport en éléments nutritifs (calcium and magnésium) indispensables aux plantes</li>
                 </ul>
                 </p>
-                <a href="https ://www.carmeuse.com/eu-en/products/agriculture-feed-and-food/terracalco"
-                    class=" lg:hidden  w-full no-underline sm:w-auto bg-blue-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
+                <a href="https://www.carmeuse.com/eu-en/products/agriculture-feed-and-food/terracalco"
+                    class=" lg:hidden  w-full no-underline sm:w-auto bg-green-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
                     <!-- <svg class="me-3 w-7 h-7" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="apple" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"></path></svg> -->
                     <div class="text-left rtl:text-right">
                         <!-- <div class="mb-1 text-xs">Download on the</div> -->
@@ -122,7 +122,7 @@
 
         </div>
     </section>
-    <section class="bg-white dark:bg-gray-900  no-border">
+    <section class="bg-white dark:bg-gray-900  no-border italic ">
         <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
             <div class="mr-auto place-self-center lg:col-span-7">
                 <h1 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">L’irrigation goutte à goutte permet la
@@ -132,8 +132,8 @@
                 <ul>
                     <p class="font-semibold"> Avantages :
                     <ul>
-                        <li>réduction de la consommation d’eau, d’énergie, d’engrais et de pesticides</li>
-                        <li>Augmentation de la production</li>
+                        <li class="c">réduction de la consommation d’eau, d’énergie, d’engrais et de pesticides</li>
+                        <li class="c">Augmentation de la production</li>
                     </ul>
                     </p>
                 </ul>
@@ -145,7 +145,7 @@
 
                 </p>
                 <a href="https ://irritec.com/"
-                    class=" lg:hidden  w-full no-underline sm:w-auto bg-blue-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
+                    class=" lg:hidden  w-full no-underline sm:w-auto bg-green-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
                     <!-- <svg class="me-3 w-7 h-7" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="apple" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"></path></svg> -->
                     <div class="text-left rtl:text-right">
                         <!-- <div class="mb-1 text-xs">Download on the</div> -->
@@ -172,8 +172,8 @@
                         <img class="" src="../imgtanzania/irritec.png" alt="" />
                     </a>
                     <div class="">
-                        <a href="https ://irritec.com/">
-                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-blue-600 dark:text-white">Irritec </h5>
+                        <a href="https ://irritec.com/"  class="no-underline">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-green-600 dark:text-white">Irritec </h5>
                         </a>
                         <!-- <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p> -->
                         <!-- <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
@@ -189,7 +189,7 @@
         </div>
     </section>
 
-    <section class="bg-white dark:bg-gray-900  no-border">
+    <section class="bg-white dark:bg-gray-900  no-border italic ">
         <div class="grid max-w-screen-xl py-4 px-8 mx-auto lg:gap-8 yl:gap-0 lg:py-16 lg:grid-cols-12">
             <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
 
@@ -199,8 +199,8 @@
                         <img class="" src="../imgtanzania/kidjanila.png" alt="" />
                     </a>
                     <div class="flex flex-col">
-                        <a href="https://kijaniland.com/">
-                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-blue-600 dark:text-white">Kijaniland</h5>
+                        <a href="https://kijaniland.com/" class="no-underline">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-green-600 dark:text-white">Kijaniland</h5>
                         </a>
                         <!-- <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p> -->
                         <!-- <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
@@ -224,7 +224,7 @@
                     Ses éléments nutritifs enrichissent et entretiennent la culture pendante et celle qui suit.
                 </p>
                 <a href="https://kijaniland.com/"
-                    class=" lg:hidden  w-full no-underline sm:w-auto bg-blue-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
+                    class=" lg:hidden  w-full no-underline sm:w-auto bg-green-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
                     <!-- <svg class="me-3 w-7 h-7" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="apple" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"></path></svg> -->
                     <div class="text-left rtl:text-right">
                         <!-- <div class="mb-1 text-xs">Download on the</div> -->
@@ -249,13 +249,13 @@
         </div>
     </section>
 
-    <section>
+    <section class="italic ">
         <div class="grid max-w-screen-xl py-4 px-8 mx-auto lg:gap-8 yl:gap-0 lg:py-16">
 
         <a href="https://abv.int/"
-            class="no-underline mx-auto flex flex-col items-center bg-blue-900 border border-blue-600  md:flex-row md:max-w-5xl hover:bg-blue-600 dark:border-blue-700 dark:bg-blue-800 dark:hover:bg-blue-700">
+            class="no-underline mx-auto flex flex-col items-center bg-green-600 border border-green-600  md:flex-row md:max-w-5xl hover:bg-green-400 dark:border-green-700 dark:bg-green-800 dark:hover:bg-green-700">
             <img class="object-cover w-full   md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
-                src="../imgtanzania/abv_anime-flag-n.png" alt="">
+                src="../imgtanzania/abv.png" alt="">
             <div class="flex flex-col justify-between p-4 leading-normal">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-white dark:text-white">ABV</h5>
                 <p class="mb-3 font-normal text-white dark:text-gray-400">ABV s’appuie sur l’espace agro économique des
@@ -270,6 +270,31 @@
     </div>
 
 </section></template>
-<style>li {
+<style>
+
+ul .a:first-child:before {
+  content: '';
+  display: inline-block;
+  height: 25px;
+  width: 35px;
+  background-image: url('../imgtanzania/true.png');
+  background-size: contain;
+  background-repeat: no-repeat;
+  margin-left: -35px;
+}
+ul .a:last-child:before {
+  content: '';
+  display: inline-block;
+  height: 25px;
+  width: 35px;
+  background-image: url('../imgtanzania/true.png');
+  background-size: contain;
+  background-repeat: no-repeat;
+  margin-left: -35px;
+}
+.b{
+    list-style: square;
+}
+.c {
     list-style: disc;
 }</style>
